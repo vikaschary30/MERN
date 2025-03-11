536b3ae716dda5147796fde0ec4e63395af9bf93
vikaschary30 <vikaschary3001@gmail.com>
2025-03-11 21:42:10 +0530

    Add files via upload

---
+++ amazon_js/ex3.js
@@ -0,0 +1,25 @@
+'My name is:';
+
+'Vikas';
+
+'My name is: '+'Vikas';
+
+'Total cost:$'+(5+3);
+
+`Total cost:$${(5+3)}`;
+
+alert(`Total cost:$${(5+3)}`);
+
+'Total cost:$'+(599+295)/100;
+
+alert(`Total cost:$${(599+295)/100}`);
+
+alert(`Total cost:$${(599+295)/100}\nThank you, come again!`);
+
+`Items (${2+2}): $${(2*2095+2*799)/100}`;
+
+`Shipping & handling: $${(499+499)/100}`;
+
+`Total before tax: $${(5788+998)/100}`;
+
+`Estimated tax(10%): $${Math.round((2*2095+2*799+499+499)*0.1)/100}`;
